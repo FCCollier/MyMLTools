143dc4f36d24b1c846819e124adebe42ed5fbd0e
FCCollier <haohsu@outlook.com>
2022-01-29 14:25:02 +0800

    新建Scrapy项目 Javdb 项目

--- javdb/javdb/settings.py
+++ javdb/javdb/settings.py
@@ -67,9 +67,9 @@ COOKIES_ENABLED = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'javdb.pipelines.JavdbPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'javdb.pipelines.PagePipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
