9402ef5da6160644223aafc30b45f36444f0da0e
FCCollier <haohsu@outlook.com>
2022-01-29 14:49:37 +0800

    新建Scrapy项目 Javdb 项目

--- javdb/javdb/pipelines.py
+++ javdb/javdb/pipelines.py
@@ -6,8 +6,28 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import MySQLdb
 
 
 class PagePipeline:
+
+    def open_spider(self, spider):
+        db_name = spider.settings.get("MYSQL_DB_NAME")
+        host = spider.settings.get("MYSQL_HOST")
+        user = spider.settings.get("MYSQL_USER")
+        pwd = spider.settings.get("MYSQL_PASSWORD")
+        self.db_conn = MySQLdb.connect(
+            db=db_name,
+            host=host,
+            user=user,
+            password=pwd
+        )
+        self.db_corsor = self.db_conn.cursor()
+
     def process_item(self, item, spider):
         return item
+
+    def close_spider(self, spider):
+        self.db_conn.commit()
+        self.db_conn.close()
+        self.db_conn.close()
