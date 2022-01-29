8f443cba230ca9ef58b9ba4f960021cc355ea6b5
FCCollier <haohsu@outlook.com>
2022-01-29 10:15:31 +0800

    新建Scrapy项目 JavBus 项目

---
+++ javbus/javbus/spiders/JavBusSpider.py
@@ -0,0 +1,16 @@
+from scrapy import Request
+from scrapy.spiders import Spider
+
+
+class JavBusSpider(Spider):
+    name = "javbus"
+
+    def start_requests(self):
+        start_urls = [
+            "https://www.busfan.club"
+        ]
+        for start_url in start_urls:
+            yield Request(url=start_url, callback=self.page_parse)
+
+    def page_parse(self, response, **kwargs):
+        print(response.status)
