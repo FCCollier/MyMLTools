0e805f78ca67cfb2ce7375895aac03115c1e3c20
FCCollier <haohsu@outlook.com>
2022-02-07 15:11:39 +0800

    新建Scrapy项目 JavBus 项目 pandas和pymysql 重写数据库读取写入部分

--- javbus/javbus/pipelines.py
+++ javbus/javbus/pipelines.py
@@ -17,29 +17,21 @@ class PagePipeline:
         )
 
     def process_item(self, item, spider):
-        try:
-            df_write = pd.DataFrame(
-                {
-                    'video_id': [item["video_id"]],
-                    'video_title': [item["video_title"]],
-                    'video_url': [item["video_url"]],
-                    'pub_date': [item["pub_date"]],
-                    'last_update': [item["last_update"]]
-                }
-            )
-            df_write.to_sql(
-                name='video_page_info',
-                con=self.engine,
-                index=False,
-                if_exists="append"
-            )
-        except KeyError as e:
-            print('键值错误：', e, '\n')
-        else:
-            print('其他类型错误')
-        finally:
-            pass
-
+        df_write = pd.DataFrame(
+            {
+                'video_id': [item["video_id"]],
+                'video_title': [item["video_title"]],
+                'video_url': [item["video_url"]],
+                'pub_date': [item["pub_date"]],
+                'last_update': [item["last_update"]]
+            }
+        )
+        df_write.to_sql(
+            name='video_page_info',
+            con=self.engine,
+            index=False,
+            if_exists="append"
+        )
         return item
 
     def close_spider(self, spider):
--- javbus/javbus/spiders/JavBusSpider.py
+++ javbus/javbus/spiders/JavBusSpider.py
@@ -3,6 +3,8 @@ from scrapy.spiders import Spider
 from ..items import PageItem
 from scrapy.loader import ItemLoader
 import time
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 class JavBusSpider(Spider):
@@ -27,24 +29,25 @@ class JavBusSpider(Spider):
     def page_parse(self, response, **kwargs):
         if response.status == 200:
             # //*[@id="waterfall"]/div
-            list_selector = response.xpath("//*[@id=‘waterfall’]/div")
+            list_selector = response.xpath("//a[@class='movie-box']")
             for one_selector in list_selector:
                 pageitem = ItemLoader(item=PageItem(), selector=one_selector)
                 # //*[@id="waterfall"]/div[1]/a/div[2]/span/date[1]
-                pageitem.add_xpath("video_id", "a/div[@class='photo-info']/span/date[1]/text()")
+                pageitem.add_xpath("video_id", "div[@class='photo-info']/span/date[1]/text()")
                 # //*[@id="waterfall"]/div[1]/a/div[1]/img
-                pageitem.add_xpath("video_title", "a/div[@class='photo-frame']/img/@title")
+                pageitem.add_xpath("video_title", "div[@class='photo-frame']/img/@title")
                 # //*[@id="waterfall"]/div[1]/a
-                pageitem.add_xpath("video_url", "a/@href")
-                pageitem.add_xpath("pub_date", "a/div[@class='photo-info']/span/date[2]/text()")
+                pageitem.add_xpath("video_url", "@href")
+                pageitem.add_xpath("pub_date", "div[@class='photo-info']/span/date[2]/text()")
                 pageitem.add_value("last_update", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-
                 yield pageitem.load_item()
             # //*[@id="next"]
             next_url = response.meta["url"] + response.xpath("//*[@id='next']/@href").extract_first()
+            print(next_url)
             yield Request(url=next_url, callback=self.page_parse, meta={"url": response.meta["url"]})
         else:
             print("索引页面到底！")
 
     def detail_parse(self, response, **kwargs):
+        # //tr[contains(@class,'result')]
         pass
