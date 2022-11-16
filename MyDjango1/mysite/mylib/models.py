7c74152afe09592f814c8974e6db6360c7bb3cf9
fccollier <fccollier@foxmail.com>
2022-11-16 16:34:42 +0800

    简易网站系统

---
+++ MyDjango1/mysite/mylib/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Actress(models.Model):
+    actress_name = models.CharField(max_length=200)
+
+
+class Video(models.Model):
+    video_id = models.AutoField(primery_key=True)
+    video_title = models.CharField(max_length=200)
+    actress_name = models.ForeignKey(Actress, on_delete=models.CASCADE)
